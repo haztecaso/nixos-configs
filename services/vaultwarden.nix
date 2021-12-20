2409b3adb9acc393fb6d46cdf812fbe1789ed6e0
Adrián Lattes Grassi <adrianlattes@disroot.org>
2021-12-20 19:36:16 +0100

    reordenando

--- services/vaultwarden.nix
+++ services/vaultwarden.nix
@@ -1,13 +1,5 @@
 { pkgs, ... }: {
   services = {
-    nginx.virtualHosts = {
-      vaultwarden = {
-        forceSSL = true;
-        enableACME = true;
-        serverName = "bw.haztecaso.com";
-        locations."/".proxyPass = "http://127.0.0.1:8222";
-      };
-    };
     vaultwarden = {
       enable = true;
       backupDir = "/srv/backups/vaultwarden"; #TODO: ensure that this folder exists and vaultwarden can write to it
@@ -16,6 +8,12 @@
         domain = "https://bw.haztecaso.com";
         rocketPort = 8222;
       };
+    };
+    nginx.virtualHosts.vaultwarden = {
+      forceSSL = true;
+      enableACME = true;
+      serverName = "bw.haztecaso.com";
+      locations."/".proxyPass = "http://127.0.0.1:8222";
     };
   };
 }
