b31c46eb720c3c3601efab1d6505b25321d8bcc9
Adrián Lattes Grassi <adrianlattes@disroot.org>
2021-12-30 18:46:04 +0100

    removed input home-manager

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs        = { url = "github:nixos/nixpkgs/release-21.11"; };
-    home-manager   = { url = "github:nix-community/home-manager"; };
+    # home-manager   = { url = "github:nix-community/home-manager"; };
     www-haztecaso  = { flake = false; url = "path:/var/www/haztecaso.com"; };
     www-lagransala = { flake = false; url = "path:/var/www/lagransala.es"; };
   };
