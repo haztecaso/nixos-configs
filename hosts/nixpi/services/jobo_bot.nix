0bbcf40315bd86fddfc2969f69b793c8e2ea57bb
Adrián Lattes <adrianlattes@disroot.org>
2022-01-05 19:27:15 +0100

    moviendo jobo_bot.age

--- hosts/nixpi/services/jobo_bot.nix
+++ hosts/nixpi/services/jobo_bot.nix
@@ -1,13 +1,10 @@
 { config, pkgs, ... }:
-let
-  config_file = ../../../secrets/jobo_bot.conf;
-in
 {
-  age.secrets."jobo_bot.conf" = ../../../secrets/jobo_bot.conf.age;
+  age.secrets."jobo_bot" = ../../../secrets/jobo_bot.conf.age;
   services.cron = {
     enable = true;
     systemCronJobs = [
-      "*/10 * * * *  ${pkgs.jobo_bot} --conf ${config.age.secrets."jobo_bot.conf".path}"
+      "*/10 * * * *  ${pkgs.jobo_bot} --conf ${config.age.secrets."jobo_bot".path}"
     ];
   };
 }
