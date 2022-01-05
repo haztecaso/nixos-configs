62d2d75223f0c32bfb239a5d001e42886f637e99
Adrián Lattes <adrianlattes@disroot.org>
2022-01-05 19:24:49 +0100

    empezando a usar agenix

--- hosts/nixpi/services/jobo_bot.nix
+++ hosts/nixpi/services/jobo_bot.nix
@@ -3,10 +3,11 @@ let
   config_file = ../../../secrets/jobo_bot.conf;
 in
 {
+  age.secrets."jobo_bot.conf" = ../../../secrets/jobo_bot.conf.age;
   services.cron = {
     enable = true;
     systemCronJobs = [
-      "*/10 * * * *  ${pkgs.jobo_bot} --conf ${config_file}"
+      "*/10 * * * *  ${pkgs.jobo_bot} --conf ${config.age.secrets."jobo_bot.conf".path}"
     ];
   };
 }
