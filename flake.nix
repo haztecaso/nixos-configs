8b998a74406fb99bcf08891b8f85139f7ed63f37
haztecaso <adrianlattes@disroot.org>
2022-01-05 21:49:56 +0100

    deploy-rs config. lo que no me mola es tener que hacer los builds en el portatil... (Ademas de que no funciona para la raspi ya que es aarch64...

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    deploy-rs.url = "github:serokell/deploy-rs";
     agenix = {
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,7 +13,7 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, agenix, utils, jobo_bot }:
+  outputs = inputs@{ self, nixpkgs, agenix, utils, jobo_bot, deploy-rs }:
   let
     overlay = final: prev: {
       jobo_bot = jobo_bot.packages.${final.system}.jobo_bot;
@@ -39,6 +40,25 @@
         modules = [ ./hosts/nixpi ];
       };
     };
+
+    deploy.nodes = {
+      lambda = {
+        hostname = "lambda";
+        profiles.system = {
+          sshUser = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.lambda;
+        };
+      };
+      nixpi = {
+        hostname = "nixpi";
+        profiles.system = {
+          sshUser = "root";
+          path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.nixpi;
+        };
+      };
+    };
+
+    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
 
   };
 }
