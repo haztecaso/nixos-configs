0ae4bc6e075c3b3c4722de7d02d6222978ef763e
Adrián Lattes Grassi <adrianlattes@disroot.org>
2022-01-30 14:16:43 +0100

    vault.haztecaso.com redirects to bw.haztecaso.com

--- modules/services/vaultwarden/default.nix
+++ modules/services/vaultwarden/default.nix
@@ -22,11 +22,19 @@ in
           rocketPort = 8222;
         };
       };
-      nginx.virtualHosts.vaultwarden = {
-        forceSSL = true;
-        enableACME = true;
-        serverName = "bw.haztecaso.com";
-        locations."/".proxyPass = "http://127.0.0.1:8222";
+      nginx.virtualHosts = {
+        vaultwarden = {
+          enableACME = true;
+          forceSSL = true;
+          serverName = "bw.haztecaso.com";
+          locations."/".proxyPass = "http://127.0.0.1:8222";
+        };
+        "vaultwarden_old" = {
+          enableACME = true;
+          forceSSL = true;
+          serverName = "vault.haztecaso.com";
+          locations."/".return = "301 https://bw.haztecaso.com$request_uri";
+        };
       };
     };
   };
