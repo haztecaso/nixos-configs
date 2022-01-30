ce82539de09adc79a5cf245fe40e03ecd1419ff9
Adrián Lattes Grassi <adrianlattes@disroot.org>
2022-01-30 17:00:59 +0100

    thumbor security key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,6 +4,7 @@ in
 with keys; {
   "configs/jobo_bot.age".publicKeys = [ skolem id_rsa_nixpi host_nixpi ];
   "credentials/cloudflare.age".publicKeys = [ skolem host_lambda ];
+  "credentials/thumbor.age".publicKeys = [ skolem host_lambda ];
   "passwords/users/skolem.age".publicKeys = hosts ++ [ skolem ];
   "passwords/users/root.age".publicKeys   = hosts ++ [ skolem ];
 }
