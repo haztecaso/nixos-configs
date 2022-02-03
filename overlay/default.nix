5de7fdb9ecd2bc1dea4902e5e3a2b6b72f1153f1
Adrián Lattes <adrianlattes@disroot.org>
2022-02-03 20:50:06 +0100

    bugfix

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,26 +1,26 @@
 final: prev: {
   deploy = prev.callPackage ./deploy.nix {};
-  autofirma = super.callPackage ./autofirma.nix {};
+  autofirma = prev.callPackage ./autofirma.nix {};
 
-  battery_level = super.callPackage ./battery_level.nix {};
+  battery_level = prev.callPackage ./battery_level.nix {};
 
-  bitwarden-rofi = super.callPackage (import (builtins.fetchGit {
+  bitwarden-rofi = prev.callPackage (import (builtins.fetchGit {
     url = "https://github.com/haztecaso/bitwarden-rofi";
     ref = "main";
     rev = "b252037ca6fd3dff4d92a2f8068f91a7049749c9";
   })) {};
 
-  configuradorfmnt = super.callPackage ./configuradorfmnt.nix {};
+  configuradorfmnt = prev.callPackage ./configuradorfmnt.nix {};
 
-  dmenu_mpd = super.callPackage ./dmenu_mpd.nix {};
+  dmenu_mpd = prev.callPackage ./dmenu_mpd.nix {};
 
-  dmenu_run_history = super.callPackage ./dmenu_run_history.nix {};
+  dmenu_run_history = prev.callPackage ./dmenu_run_history.nix {};
 
-  dnsblock = super.callPackage ./dnsblock.nix {};
+  dnsblock = prev.callPackage ./dnsblock.nix {};
 
-  message = super.callPackage ./message.nix {};
+  message = prev.callPackage ./message.nix {};
 
-  uni-cd = super.callPackage ./uni-cd.nix {};
+  uni-cd = prev.callPackage ./uni-cd.nix {};
 
-  uni-schedule = super.callPackage ./uni-schedule.nix {};
+  uni-schedule = prev.callPackage ./uni-schedule.nix {};
 }
