8b6d23a4e1bbdbe7d79566a4a020be4f0002e256
haztecaso <adrianlattes@disroot.org>
2022-02-08 12:02:17 +0100

    rekeying secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   keys = import ../ssh-keys.nix;
 in
 with keys; {
-  "jobo_bot.age".publicKeys = [ skolem id_rsa_nixpi host_nixpi ];
+  "jobo_bot.age".publicKeys = [ skolem id_rsa_nixpi host_nixpi host_lambda ];
   "cloudflare.age".publicKeys = [ skolem host_lambda ];
   "thumbor.age".publicKeys = [ skolem host_lambda ];
   "users/skolem.age".publicKeys = hosts ++ [ skolem ];
