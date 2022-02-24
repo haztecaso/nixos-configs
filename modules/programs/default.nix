869e1370e522b8bcd2d4aad77950e3c53ff7fd8b
Adrián Lattes <adrianlattes@disroot.org>
2022-02-24 15:35:29 +0100

    Adding defaultApplications

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -1,5 +1,34 @@
 { config, lib, pkgs, ... }:
 let
+  home-config = {
+    xdg.mimeApps = {
+      enable = true;
+      associations.added = {
+        "x-scheme-handler/magnet" = "transmission-gtk.desktop";
+        "x-scheme-handler/tg" = "telegramdesktop.desktop";
+        "application/pdf"="org.pwmt.zathura.desktop";
+        "audio/flac"="mpv.desktop";
+      };
+      defaultApplications = {
+        "x-scheme-handler/magnet" = "transmission-gtk.desktop";
+        "application/pdf" = "org.pwmt.zathura.desktop";
+        "audio/flac" = "mpv.desktop";
+        "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
+        "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
+        "text/html" = "org.qutebrowser.qutebrowser.desktop";
+        "image/gif" = "sxiv.desktop";
+        "image/jpeg" = "sxiv.desktop";
+        "image/png" = "sxiv.desktop";
+        "image/tiff" = "sxiv.desktop";
+        "application/x-bzip" = "xarchiver.desktop";
+        "application/x-bzip2" = "xarchiver.desktop";
+        "application/zip" = "xarchiver.desktop";
+        "application/gzip" = "xarchiver.desktop";
+        "application/x-7z-compressed" = "xarchiver.desktop";
+        "application/x-tar" = "xarchiver.desktop";
+      };
+    };
+  };
 in
 {
   imports = [
@@ -9,4 +38,6 @@ in
     ./tmux
     ./vim
   ];
+  home-manager.users.skolem = { ... }: home-config;
+  home-manager.users.root   = { ... }: home-config;
 }
