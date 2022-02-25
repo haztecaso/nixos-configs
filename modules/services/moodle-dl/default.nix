49213bdd405645617e62769fb195ea64c637dcf3
Adrián Lattes Grassi <adrianlattes@disroot.org>
2022-02-25 15:02:03 +0100

    Adding moodle-dl config

--- modules/services/moodle-dl/default.nix
+++ modules/services/moodle-dl/default.nix
@@ -1,8 +1,25 @@
-{ config, lib, pkgs, ... }: {
-  options.custom.services.moodle-dl = {
-    enable = lib.mkEnableOption "Enable moodle downloader service";
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.custom.services.moodle-dl;
+  i2s = lib.strings.floatToString;
+in
+{
+  options.custom.services.moodle-dl = with lib; {
+    enable = mkEnableOption "Enable moodle downloader service";
+    frequency = mkOption {
+      type = types.int;
+      default = 10;
+      description = "frequency of cron job in minutes";
+    };
   };
   config = lib.mkIf config.custom.services.moodle-dl.enable {
-      environment.systemPackages = [ pkgs.moodle-dl ];
+    environment.systemPackages = with pkgs; [ moodle-dl ];
+    services.cron = {
+      enable = true;
+      systemCronJobs = [
+        ''*/${i2s cfg.frequency} * * * *  root . /etc/profile; ${pkgs.moodle-dl}/bin/moodle-dl -c ${config.age.secrets."moodle-dl.conf"}''
+      ];
+    };
+    age.secrets."moodle-dl.conf".file = ../../../secrets/moodle-dl.age;
   };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,6 +3,7 @@ let
 in
 with keys; {
   "jobo_bot.age".publicKeys = [ skolem id_rsa_nixpi host_nixpi host_lambda ];
+  "moodle-dl.age".publicKeys = [ skolem id_rsa_nixpi host_nixpi host_lambda ];
   "cloudflare.age".publicKeys = [ skolem host_lambda ];
   "thumbor.age".publicKeys = [ skolem host_lambda ];
   "users/skolem.age".publicKeys = hosts ++ [ skolem ];
