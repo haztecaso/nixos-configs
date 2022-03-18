0df7451eb8c5e056e3541936bf8aa5d7d9ab03f1
Adrián Lattes <adrianlattes@disroot.org>
2022-03-18 18:05:37 +0100

    Configurando irc

--- modules/programs/irssi/default.nix
+++ modules/programs/irssi/default.nix
@@ -5,6 +5,11 @@ in
 {
   options.custom.programs.irssi = with lib; {
     enable = mkEnableOption "Enable irssi irc client.";
+    nick = mkOption {
+      type = types.str;
+      default = "tarski";
+      description = "irc nick";
+    };
   };
 
   config = lib.mkIf cfg.enable (let
@@ -13,29 +18,32 @@ in
         enable = true;
         networks = {
           liberachat = {
-            nick = "skolem";
+            nick = cfg.nick;
+            autoCommands = [
+              "/msg "
+            ];
             server = {
               address = "irc.libera.chat";
               port = 6697;
               ssl.enable = true;
-              autoConnect = true;
+              # autoConnect = true;
             };
             channels = {
               nixos.autoJoin = true;
             };
           };
-          irchighway = {
-            nick = "skolem";
-            server = {
-              address = "irc.irchighway.net";
-              port = 6697;
-              ssl.enable = true;
-              autoConnect = true;
-            };
-            channels = {
-              ebooks.autoJoin = true;
-            };
-          };
+          # irchighway = {
+          #   nick = cfg.nick;
+          #   server = {
+          #     address = "irc.irchighway.net";
+          #     port = 7000;
+          #     ssl.enable = false;
+          #     autoConnect = true;
+          #   };
+          #   channels = {
+          #     ebooks.autoJoin = true;
+          #   };
+          # };
         };
       };
     };
