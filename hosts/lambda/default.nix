655424a32a4842ea2dd52a6d4875b19d639dad34
Adrián Lattes Grassi <adrianlattes@disroot.org>
2022-04-02 18:01:35 +0200

    Removing old alias

--- hosts/lambda/default.nix
+++ hosts/lambda/default.nix
@@ -21,7 +21,6 @@
       shells.aliases = {
         ".." = "cd ..";
         less = "less --quit-if-one-screen --ignore-case --status-column --LONG-PROMPT --RAW-CONTROL-CHARS --HILITE-UNREAD --tabs=4 --no-init --window=-4";
-        r = "ranger";
         cp = "cp -i";
         agenix = "nix run github:ryantm/agenix --";
       };
