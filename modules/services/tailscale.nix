0a24d8cd531db26fa072c5c6b9913f696359f96a
Adrián Lattes <adrianlattes@disroot.org>
2022-04-12 16:56:14 +0200

    tailscale hosts

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -1,15 +1,33 @@
 { config, lib, pkgs, ... }:
 let
   cfg = config.services.custom.tailscale;
+  localNames =  name: [
+    name
+    "${name}.lan"
+    "${name}.local"
+  ];
 in
 {
   options.services.custom.tailscale = with lib; {
     enable = mkEnableOption "Tailscale service and package.";
+    hosts = mkOption {
+      type = types.attrsOf (types.listOf types.str);
+      description = "Tailscale /etc/hosts entries.";
+      default = {
+        "100.84.40.96"   = localNames "lambda";
+        "100.75.165.118" = localNames "beta";
+        "100.70.238.47"  = localNames "realme8";
+        "100.84.161.27"  = localNames "galois";
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [ pkgs.tailscale ];
     services.tailscale.enable = true;
-    networking.firewall.allowedUDPPorts = [ 41641 ];
+    networking = {
+      firewall.allowedUDPPorts = [ 41641 ];
+      hosts = cfg.hosts;
+    };
   };
 }
