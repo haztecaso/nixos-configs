a2b91c195a94d1fdaec5b9811e62244e5ad3d718
Adrián Lattes Grassi <adrianlattes@disroot.org>
2022-04-20 02:10:44 +0200

    Gitea mail server config

--- modules/webserver/gitea.nix
+++ modules/webserver/gitea.nix
@@ -7,11 +7,29 @@
       gitea = {
         enable = true;
         domain = "git.haztecaso.com";
+        appName = "Gitea";
+        cookieSecure = true;
+        disableRegistration = true;
         httpAddress = "127.0.0.1";
         httpPort = 8003;
         rootUrl = "https://git.haztecaso.com/";
-        dump = {
-          enable = true;
+        dump.enable = true;
+        settings = {
+          security = {
+            LOGIN_REMEMBER_DAYS = 14;
+            MIN_PASSWORD_LENGTH = 12;
+            PASSWORD_COMPLEXITY = "lower,upper,digit,spec";
+            PASSWORD_CHECK_PWN = true;
+          };
+          mailer = {
+            ENABLED = true;
+            MAILER_TYPE = "sendmail";
+            FROM = "gitea@haztecaso.com";
+            SENDMAIL_PATH = "${pkgs.system-sendmail}/bin/sendmail";
+          };
+          other = {
+            SHOW_FOOTER_VERSION = false;
+          };
         };
       };
       nginx.virtualHosts.gitea = {
@@ -20,6 +38,11 @@
         serverName = "git.haztecaso.com";
         locations."/".proxyPass = "http://127.0.0.1:8003";
       };
+    };
+    mailserver.loginAccounts."gitea@haztecaso.com" = {
+      hashedPassword = "$2y$05$YHivXvJqLKBlXuddFkGpiev087Ve0DsJwyvKJFUsEfB31ULkZPS5O";
+      name = "Gitea";
+      sendOnly = true;
     };
   };
 }
