3f840590c669efdb0b8d156314dc52a9db66d778
Adrián Lattes <adrianlattes@disroot.org>
2022-06-22 01:45:31 +0200

    vim home module

--- modules/home/vim.nix
+++ modules/home/vim.nix
@@ -1,41 +1,36 @@
 { lib, pkgs, config, ... }:
 let
   cfg = config.custom.programs.vim;
-  mkHomeConfig = conf: {
-    home-manager.users = {
-      skolem = { ... }: conf;
-      root = { ... }: conf;
-    };
-  };
 in
 {
   options.custom.programs.vim = with lib; {
     enable = mkOption {
       type = types.bool;
       default = true;
-      description = "Enable neovim editor";
+      description = "Wether to enable neovim editor.";
     };
     package = mkOption {
       type = types.package;
       default = pkgs.neovimBase;
-      description = "Neovim package to install";
+      description = "Neovim package to install.";
     };
     defaultEditor = mkOption {
       type = types.bool;
       default = true;
-      description = "When enabled configs [neo]vim as the default editor using the EDITOR environment variable.";
+      description = "When enabled sets [neo]vim as the default editor using the EDITOR environment variable.";
     };
   };
 
   config = lib.mkMerge [
-    (lib.mkIf cfg.enable (mkHomeConfig {
+    (lib.mkIf cfg.enable {
       home.packages = [ cfg.package ];
-    }))
-    (lib.mkIf (cfg.defaultEditor) (mkHomeConfig {
+    })
+
+    (lib.mkIf (cfg.defaultEditor) {
       home.sessionVariables = {
         EDITOR = "nvim";
         VISUAL = "nvim";
       };
-    }))
+    })
   ];
 }
