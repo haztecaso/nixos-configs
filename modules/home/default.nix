76a67742e94bb2a0e425c284489ddc04fbecd10a
Adrián Lattes <adrianlattes@disroot.org>
2022-06-22 02:13:11 +0200

    stateVersionModule

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,49 +1,56 @@
-{ config, nixosConfig, ... }:
+{ config, ... }:
+let
+  stateVersionModule = { nixosConfig, ... }: {
+    home.stateVersion = nixosConfig.base.stateVersion;
+  };
+in
 {
   home-manager = {
     extraSpecialArgs = { nixosConfig = config; };
     useGlobalPkgs = true;
-    sharedModules = [{
-      imports = [
-        ./latex.nix
-        ./mail.nix
-        ./music.nix
-        ./ssh.nix
-        ./tmux.nix
-        ./vim.nix
-        ./monitors.nix
-      ];
-      home.stateVersion = nixosConfig.base.stateVersion;
-      xdg.mimeApps = {
-        enable = true;
-        associations.added = {
-          "x-scheme-handler/magnet" = "transmission-gtk.desktop";
-          "x-scheme-handler/tg" = "telegramdesktop.desktop";
-          "application/pdf" = "org.pwmt.zathura.desktop";
-          "audio/flac" = "mpv.desktop";
+    sharedModules = [
+      stateVersionModule
+      {
+        imports = [
+          ./latex.nix
+          ./mail.nix
+          ./music.nix
+          ./ssh.nix
+          ./tmux.nix
+          ./vim.nix
+          ./monitors.nix
+        ];
+        xdg.mimeApps = {
+          enable = true;
+          associations.added = {
+            "x-scheme-handler/magnet" = "transmission-gtk.desktop";
+            "x-scheme-handler/tg" = "telegramdesktop.desktop";
+            "application/pdf" = "org.pwmt.zathura.desktop";
+            "audio/flac" = "mpv.desktop";
+          };
+          defaultApplications = {
+            "x-scheme-handler/magnet" = "transmission-gtk.desktop";
+            "application/pdf" = "org.pwmt.zathura.desktop";
+            "audio/flac" = "mpv.desktop";
+            "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
+            "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
+            "x-scheme-handler/chrome" = "org.qutebrowser.qutebrowser.desktop";
+            "application/x-extension-htm" = "org.qutebrowser.qutebrowser.desktop";
+            "application/x-extension-html" = "org.qutebrowser.qutebrowser.desktop";
+            "text/html" = "org.qutebrowser.qutebrowser.desktop";
+            "image/gif" = "sxiv.desktop";
+            "image/jpeg" = "sxiv.desktop";
+            "image/png" = "sxiv.desktop";
+            "image/tiff" = "sxiv.desktop";
+            "application/x-bzip" = "xarchiver.desktop";
+            "application/x-bzip2" = "xarchiver.desktop";
+            "application/zip" = "xarchiver.desktop";
+            "application/gzip" = "xarchiver.desktop";
+            "application/x-7z-compressed" = "xarchiver.desktop";
+            "application/x-tar" = "xarchiver.desktop";
+          };
         };
-        defaultApplications = {
-          "x-scheme-handler/magnet" = "transmission-gtk.desktop";
-          "application/pdf" = "org.pwmt.zathura.desktop";
-          "audio/flac" = "mpv.desktop";
-          "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
-          "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
-          "x-scheme-handler/chrome" = "org.qutebrowser.qutebrowser.desktop";
-          "application/x-extension-htm" = "org.qutebrowser.qutebrowser.desktop";
-          "application/x-extension-html" = "org.qutebrowser.qutebrowser.desktop";
-          "text/html" = "org.qutebrowser.qutebrowser.desktop";
-          "image/gif" = "sxiv.desktop";
-          "image/jpeg" = "sxiv.desktop";
-          "image/png" = "sxiv.desktop";
-          "image/tiff" = "sxiv.desktop";
-          "application/x-bzip" = "xarchiver.desktop";
-          "application/x-bzip2" = "xarchiver.desktop";
-          "application/zip" = "xarchiver.desktop";
-          "application/gzip" = "xarchiver.desktop";
-          "application/x-7z-compressed" = "xarchiver.desktop";
-          "application/x-tar" = "xarchiver.desktop";
-        };
-      };
-    }];
+      }
+    ];
   };
 }
