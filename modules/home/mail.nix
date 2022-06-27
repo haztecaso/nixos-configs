6292de6d052ac10af450ea0ec64e56abcd3e21a0
Adrián Lattes <adrianlattes@disroot.org>
2022-06-28 01:08:03 +0200

    Updating mail module

--- modules/home/mail.nix
+++ modules/home/mail.nix
@@ -1,6 +1,32 @@
+# Idea for mail deletion: https://blog.tomecek.net/post/removing-messages-with-notmuch/
+
 { config, lib, pkgs, ... }:
 let
   cfg = config.custom.mail;
+  mkConfig = name: { address, imapAddress, smtpAddress, primary ? false }: {
+    inherit address primary;
+    realName = "Adrián Lattes Grassi";
+    userName = address;
+
+    passwordCommand = "${pkgs.pass}/bin/pass show mails/${address}";
+
+    imap = { host = imapAddress; port = 993; };
+    smtp = { host = imapAddress; port = 465; };
+
+    mbsync = { enable = true; create = "maildir"; };
+
+    imapnotify = {
+      enable = true;
+      boxes = ["Inbox" "Sent" "Drafts"];
+      onNotify = "${pkgs.isync}/bin/mbsync -q ${name}; ${pkgs.notmuch}/bin/notmuch new";
+      onNotifyPost = "${pkgs.dunst}/bin/dunstify -r 1003 'New mail' '${name}'";
+    };
+
+    mbsync.extraConfig.account.PipelineDepth = 10;
+    msmtp.enable = true;
+    notmuch.enable = true;
+    astroid.enable = true;
+  };
 in {
   options.custom.mail = with lib; {
     enable = mkEnableOption "mail";
@@ -24,9 +50,34 @@ in {
     };
   };
   config = lib.mkIf cfg.enable {
+    accounts.email.accounts = lib.mapAttrs mkConfig cfg.accounts;
+    home.packages = with pkgs; [ pass ];
     programs = {
       mbsync.enable = true; # Download mails with imap
       msmtp.enable = true;  # Send mails
+      password-store.enable = true;
+
+      # Tag and search mails
+      notmuch = {
+        enable = true;
+        maildir.synchronizeFlags = true;
+        new.tags = [ "new" ];
+        hooks.postNew = "${pkgs.afew}/bin/afew -tnv";
+      };
+
+      # notmuch autotagging and filters
+      afew = {
+        enable = true;
+        extraConfig =
+          # ${filters}
+          ''
+            [SpamFilter]
+            [KillThreadsFilter]
+            [ListMailsFilter]
+            [ArchiveSentMailsFilter]
+            [InboxFilter]
+          '';
+        };
 
       # CLI mail client
       alot = {
@@ -52,6 +103,17 @@ in {
           };
         };
       };
+
+      # mailcap for alot
+      home.file.".mailcap".text = ''
+        text/plain; cat %s
+        text/html; ${pkgs.w3m}/bin/w3m -dump -o document_charset=%{charset} '%s'; nametemplate=%s.html; copiousoutput
+        video/mp4; ${pkgs.mpv}/bin/mpv %s
+        application/pdf; zathura %s
+        application/pdf:pdf; zathura %s
+        image/*; sxiv %s
+      '';
+
     };
   };
 }
