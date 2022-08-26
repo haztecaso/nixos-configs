5738de7ec34af9bd06bbb5af9c090b6d204b7bf4
haztecaso <adrianlattes@disroot.org>
2022-08-26 12:13:20 +0200

    Ajustando valores elbrus

--- hosts/elbrus/hardware.nix
+++ hosts/elbrus/hardware.nix
@@ -7,7 +7,7 @@
     initrd = {
       availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
       kernelModules = [ ];
-      luks.devices."root".device = "/dev/disk/by-uuid/48eab118-2d67-41da-846a-d9c6c49a37b7";
+      luks.devices."root".device = "/dev/disk/by-uuid/941231a1-cfad-40c7-a2ea-0cbcba781ee2";
     };
 
     kernelModules = [ "wl" ];
@@ -31,11 +31,11 @@
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/15CC-2C93";
+    device = "/dev/disk/by-uuid/9B95-F0B7";
     fsType = "vfat";
   };
 
-  swapDevices = [{ device = "/dev/disk/by-uuid/48eab118-2d67-41da-846a-d9c6c49a37b7"; }];
+  swapDevices = [{ device = "/dev/disk/by-uuid/632db255-52bf-4c8c-b4da-30b29bb25a7c"; }];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
