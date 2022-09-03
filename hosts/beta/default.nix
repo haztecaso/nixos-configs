2af6c20aa691611a448b68f31923c6b527ffa7c3
Adrián Lattes <adrianlattes@disroot.org>
2022-09-04 00:56:26 +0200

    Autofs funcionando

--- hosts/beta/default.nix
+++ hosts/beta/default.nix
@@ -129,6 +129,7 @@ in
 
     services = {
       tailscale.enable = true;
+      autofs.enable = true;
     };
 
     dev = {
--- modules/services/autofs.nix
+++ modules/services/autofs.nix
@@ -1,20 +1,24 @@
 { config, lib, pkgs, ... }:
 let
-  cfg = config.custom.services.vaultwarden;
+  cfg = config.custom.services.autofs;
 in
 {
   options.custom.services.autofs = with lib; {
     enable = mkEnableOption "Enable autofs automounting daemon.";
   };
   config = lib.mkIf cfg.enable {
-      services.autofs = {
-          enable = true;
-          autoMaster = let 
-            mapConf = pkgs.writeText "auto" ''
-              nas -fstype=fuse,rw,nodev,nonempty,noatime,allow_other,max_read=65536 :ssfs\#root@myserver.example.com\:/mnt/raid
-            '';
-          in ''
+    services.autofs = {
+        enable = true;
+        autoMaster = let 
+          options="-fstype=fuse,rw,sync,nodev,allow_other,follow_symlinks ";
+          sshfs="${pkgs.sshfs}/bin/sshfs";
+          mapConf = pkgs.writeText "auto" ''
+            nas ${options} :${sshfs}\#root@nas\:/mnt/raid
           '';
-      };
+        in ''
+          /mnt/auto/ file:${mapConf} -v
+      '';
+    };
+    environment.systemPackages = [ pkgs.sshfs ];
   };
 }
