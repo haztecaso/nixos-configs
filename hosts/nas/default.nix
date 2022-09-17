8baca8a90b3c3355e76ced5bb7f2b7073cec05d2
haztecaso <adrianlattes@disroot.org>
2022-09-18 00:56:50 +0200

    Módulo musica y conf NAS

--- hosts/nas/default.nix
+++ hosts/nas/default.nix
@@ -23,9 +23,9 @@
       custom.programs = {
         tmux.color = "#aaee00";
         vim.package = vim;
-        music.server = {
+        music = {
           enable = true;
-          library = /mnt/raid/music/Library;
+          library = "/mnt/raid/music/Library";
         };
       };
     };
@@ -35,6 +35,7 @@
     hostname = "nas";
     hostnameSymbol = "ν";
     stateVersion = "21.05";
+    printing = true;
   };
 
   programs = {
@@ -44,6 +45,10 @@
   custom = {
     services = {
         tailscale.enable = true;
+        music-server = {
+          enable = true;
+          library = "/mnt/raid/music/Library";
+        };
     };
   };
 
@@ -72,6 +77,16 @@
     };
   };
 
+  services.printing.browsing = true;
+  services.printing.listenAddresses = [ "*:631" ]; # Not 100% sure this is needed and you might want to restrict to the local network
+  services.printing.allowFrom = [ "all" ]; # this gives access to anyone on the interface you might want to limit it see the official documentation
+  services.printing.defaultShared = true; # If you want
+
+  services.avahi.enable = true;
+  services.avahi.publish.enable = true;
+  services.avahi.publish.userServices = true;
+
+
   services.hydra = {
     enable = true;
     hydraURL = "http://nas:3000";
@@ -81,8 +96,8 @@
   };
 
   networking.firewall = {
-    allowedTCPPorts = [ 80 443 111 2049 3000 4000 8000 4001 4002 20048 ];
-    allowedUDPPorts = [ 80 443 111 2049 3000 4000 8000 4001 4002 20048 ];
+    allowedTCPPorts = [ 80 631 443 111 2049 3000 4000 8000 4001 4002 20048 ];
+    allowedUDPPorts = [ 80 631 443 111 2049 3000 4000 8000 4001 4002 20048 ];
   };
 
 }
--- modules/home/music.nix
+++ modules/home/music.nix
@@ -1,112 +1,68 @@
 { lib, pkgs, config, ... }:
 let
   cfg = config.custom.programs.music;
-  commonConfig = {
-      home.packages = [ pkgs.mpc_cli ];
-      programs.ncmpcpp = {
-        enable = true;
-        settings = {
-          centered_cursor = true;
-          user_interface = "alternative";
-        };
-        bindings = [
-          { key = "Q"; command = "quit"; }
-          { key = "q"; command = "dummy"; }
-          { key = "="; command = "show_clock"; }
-          { key = "+"; command = "volume_up"; }
-          { key = "j"; command = "scroll_down"; }
-          { key = "k"; command = "scroll_up"; }
-          { key = "ctrl-u"; command = "page_up"; }
-          { key = "ctrl-d"; command = "page_down"; }
-          { key = "u"; command = "page_up"; }
-          { key = "d"; command = "page_down"; }
-          { key = "h"; command = "previous_column"; }
-          { key = "l"; command = "next_column"; }
-          { key = "."; command = "show_lyrics"; }
-          { key = "n"; command = "next_found_item"; }
-          { key = "N"; command = "previous_found_item"; }
-          { key = "J"; command = "move_sort_order_down"; }
-          { key = "K"; command = "move_sort_order_up"; }
-          { key = "h"; command = "jump_to_parent_directory"; }
-          { key = "l"; command = "enter_directory"; }
-          { key = "l"; command = "run_action"; }
-          { key = "l"; command = "play_item"; }
-          { key = "m"; command = "show_media_library"; }
-          { key = "m"; command = "toggle_media_library_columns_mode"; }
-          { key = "t"; command = "show_tag_editor"; }
-          { key = "v"; command = "show_visualizer"; }
-          { key = "G"; command = "move_end"; }
-          { key = "g"; command = "move_home"; }
-          { key = "U"; command = "update_database"; }
-          { key = "s"; command = "reset_search_engine"; }
-          { key = "s"; command = "show_search_engine"; }
-          { key = "f"; command = "show_browser"; }
-          { key = "f"; command = "change_browse_mode"; }
-          { key = "x"; command = "delete_playlist_items"; }
-          { key = "P"; command = "show_playlist"; }
-        ];
-      };
-    };
 in
 {
   options.custom.programs.music = with lib; {
     enable = mkEnableOption "Enable music setup (nas: mpd server, clients: consumers).";
-    client = {
-      enable = mkEnableOption "Enable client (music player) config.";
-    };
-    server = {
-      enable = mkEnableOption "Enable server (music streamer) config.";
-      library = mkOption {
-        type = types.path;
-        description = "Music libary directory (read by mpd, beets)."; 
-      };
-      playlistDir = mkOption {
-        type = types.path;
-        default = cfg.server.library + "/Playlists";
-        description = "Mpd music directory";
-      };
-      mpdDbFile = mkOption {
-        type = types.path;
-        default = cfg.server.library + "/mpd.db";
-        description = "Mpd database path";
-      };
-      mpdDataDir = mkOption {
-        type = types.path;
-        default = cfg.server.library + "/.mpd";
-        description = "Mpd database path";
-      };
+    library = mkOption {
+      type = types.path;
+      description = "Music libary directory."; 
     };
   };
 
-  config = lib.mkMerge [
-    (lib.mkIf cfg.server.enable ( commonConfig // {
-      services.mpd = {
-        enable = true;
-        musicDirectory = cfg.server.library;
-        playlistDirectory = cfg.server.playlistDir;
-        dataDir = cfg.server.mpdDataDir;
-        dbFile = builtins.toString cfg.server.mpdDbFile;
-        extraConfig = ''
-          audio_output {
-            type "httpd"
-            name "NAS"
-            encoder "lame"
-            server "0.0.0.0"
-            port "8000"
-            bitrate "160"
-          }
-        '';
+  config = (lib.mkIf cfg.enable {
+    home.packages = [ pkgs.mpc_cli ];
+    programs.ncmpcpp = {
+      enable = true;
+      settings = {
+        mpd_host = "nas";
+        centered_cursor = true;
+        user_interface = "alternative";
       };
-    }))
-    (lib.mkIf cfg.client.enable ( commonConfig // {
-      services.mpdris2 = {
-        enable = true;
-        mpd.host = "nas";
-      };
-      programs.ncmpcpp = {
-        mpdMusicDir = cfg.library;
-        settings.mpd_host = "nas";
-      };
-    }))
-  ];
+      bindings = [
+        { key = "Q"; command = "quit"; }
+        { key = "q"; command = "dummy"; }
+        { key = "="; command = "show_clock"; }
+        { key = "+"; command = "volume_up"; }
+        { key = "j"; command = "scroll_down"; }
+        { key = "k"; command = "scroll_up"; }
+        { key = "ctrl-u"; command = "page_up"; }
+        { key = "ctrl-d"; command = "page_down"; }
+        { key = "u"; command = "page_up"; }
+        { key = "d"; command = "page_down"; }
+        { key = "h"; command = "previous_column"; }
+        { key = "l"; command = "next_column"; }
+        { key = "."; command = "show_lyrics"; }
+        { key = "n"; command = "next_found_item"; }
+        { key = "N"; command = "previous_found_item"; }
+        { key = "J"; command = "move_sort_order_down"; }
+        { key = "K"; command = "move_sort_order_up"; }
+        { key = "h"; command = "jump_to_parent_directory"; }
+        { key = "l"; command = "enter_directory"; }
+        { key = "l"; command = "run_action"; }
+        { key = "l"; command = "play_item"; }
+        { key = "m"; command = "show_media_library"; }
+        { key = "m"; command = "toggle_media_library_columns_mode"; }
+        { key = "t"; command = "show_tag_editor"; }
+        { key = "v"; command = "show_visualizer"; }
+        { key = "G"; command = "move_end"; }
+        { key = "g"; command = "move_home"; }
+        { key = "U"; command = "update_database"; }
+        { key = "s"; command = "reset_search_engine"; }
+        { key = "s"; command = "show_search_engine"; }
+        { key = "f"; command = "show_browser"; }
+        { key = "f"; command = "change_browse_mode"; }
+        { key = "x"; command = "delete_playlist_items"; }
+        { key = "P"; command = "show_playlist"; }
+      ];
+    };
+    services.mpdris2 = {
+      enable = true;
+      mpd.host = "nas";
+    };
+    programs.ncmpcpp = {
+      mpdMusicDir = cfg.library;
+    };
+  });
 }
