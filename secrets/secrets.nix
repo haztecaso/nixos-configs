3864b2227adb94cc5b7b0777814da1915f85e18a
Adrián Lattes Grassi <adrianlattes@disroot.org>
2022-12-08 15:57:17 +0100

    Eliminando clave antigua

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,8 +2,9 @@ let
   keys = import ../ssh-keys.nix;
 in
 with keys; {
-  "jobo_bot.age".publicKeys = [ skolem id_rsa_nixpi host_nixpi host_lambda ];
-  "moodle-dl.age".publicKeys = [ skolem id_rsa_nixpi host_nixpi host_lambda ];
+  "jobo_bot.age".publicKeys = [ skolem host_lambda ];
+  "remadbot.age".publicKeys = [ skolem host_lambda ];
+  "moodle-dl.age".publicKeys = [ skolem host_lambda ];
   "cloudflare.age".publicKeys = [ skolem host_lambda ];
   "thumbor.age".publicKeys = [ skolem host_lambda ];
   "users/skolem.age".publicKeys = hosts ++ [ skolem ];
