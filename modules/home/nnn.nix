2ebdc491d9277fd8f46b7e42bd5c8a5b154e39a8
haztecaso <adrianlattes@disroot.org>
2023-01-27 16:49:26 +0100

    Arreglando defaults nnn bookmarks

--- modules/home/nnn.nix
+++ modules/home/nnn.nix
@@ -2,54 +2,56 @@
 let
   cfg = config.custom.programs.nnn;
 in
-  {
-    options.custom.programs.nnn = with lib; {
-      enable = mkOption {
-        type = types.bool;
-        default = true;
-        description = "Enable custom nnn config with shortcuts";
-      };
-
-      bookmarks = mkOption {
-        type = with types; attrsOf str;
-        description = "Directory bookmarks.";
-        default = {
-          d = "~/Documents";
-          D = "~/Downloads";
-          p = "~/Pictures";
-          h = "~";
-          c = "~/.config";
-          m = "~/Music";
-          v = "~/Videos";
-        };
-      };
+{
+  options.custom.programs.nnn = with lib; {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable custom nnn config with shortcuts";
     };
 
-    config = lib.mkIf cfg.enable {
-      programs = {
-        nnn = {
-          enable = true;
-          package = pkgs.nnn.override ({ withNerdIcons = true; });
-          extraPackages = with pkgs; [ tabbed sxiv ];
-          plugins = {
-            src = "${inputs.nnn}/plugins";
-            mappings = {
-              P = "preview-tabbed";
-              f = "finder";
-              p = "preview-tui";
-              v = "imgview";
-            };
+    bookmarks = mkOption {
+      type = with types; attrsOf str;
+      description = "Directory bookmarks.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs = {
+      nnn = {
+        enable = true;
+        package = pkgs.nnn.override ({ withNerdIcons = true; });
+        extraPackages = with pkgs; [ tabbed sxiv ];
+        plugins = {
+          src = "${inputs.nnn}/plugins";
+          mappings = {
+            P = "preview-tabbed";
+            f = "finder";
+            p = "preview-tui";
+            v = "imgview";
           };
-          bookmarks = cfg.bookmarks;
         };
+        bookmarks = cfg.bookmarks;
+      };
     };
-    custom.shell = {
-      initExtra = [
-        ''
-          export NNN_OPTS="aRe"
-        ''
-      ];
-      aliases.r = "nnn";
+    custom = {
+      shell = {
+        initExtra = [
+          ''
+            export NNN_OPTS="aRe"
+          ''
+        ];
+        aliases.r = "nnn";
+      };
+      programs.nnn.bookmarks = with lib;{
+        d = mkDefault "~/Documents";
+        D = mkDefault "~/Downloads";
+        p = mkDefault "~/Pictures";
+        h = mkDefault "~";
+        c = mkDefault "~/.config";
+        m = mkDefault "~/Music";
+        v = mkDefault "~/Videos";
+      };
     };
   };
 }
