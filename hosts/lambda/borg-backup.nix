7b5c2eb2c42036041610a0800caa2a0fa7a96b79
haztecaso <adrianlattes@disroot.org>
2023-03-05 22:58:57 +0100

    Desactivando backups por ahora hasta que funcionen

--- hosts/lambda/borg-backup.nix
+++ hosts/lambda/borg-backup.nix
@@ -1,13 +1,23 @@
+# TODO:
+# Conseguir que el servicio de borg funcione
+# Incluir backups de:
+# - vaultwarden
+# - gitea
+# - bases de datos: mysql
+# - matomo
+
 { config, pkgs, ... }: {
-  services.borgbackup.jobs = {
-    webs = {
-      paths = "/var/www/";
-      exclude = [ "/var/www/haztecaso.com/radio-old/" ];
-      encryption.mode = "none"; 
-      environment.BORG_RSH = "ssh -i /home/skolem/.ssh/id_rsa";
-      repo = "ssh://skolem@nas/mnt/raid/backups/borg/lambda";
-      compression = "auto,zstd";
-      startAt = "daily";
-    };
-  };
+  # environment.systemPackages = [ pkgs.borgbackup ];
+  # services.borgbackup.jobs = {
+  #   webs = {
+  #     paths = "/var/www/";
+  #     exclude = [ "/var/www/haztecaso.com/radio-old/" ];
+  #     encryption.mode = "none"; 
+  #     environment.BORG_RSH = "ssh -i /home/skolem/.ssh/id_rsa";
+  #     repo = "ssh://skolem@nas:22/mnt/raid/backups/borg/lambda";
+  #     compression = "auto,zstd";
+  #     # startAt = "*:*:0"; # For debuggin
+  #     startAt = "0/6:0:0";
+  #   };
+  # };
 }
