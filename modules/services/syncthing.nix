665083dc172fd0cc2790f8fe17b5a44aa853147e
haztecaso <adrianlattes@disroot.org>
2023-05-03 11:51:28 +0200

    Añadiendo elbrus y vault

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -6,6 +6,7 @@ let
     macbook.id = "JE3TRVV-CSMT42I-YUPYLOR-PXKMXSR-GKN3APW-SAHMJRM-JVCX66N-4BBRWQJ";
     realmi8.id = "GGY5ZY5-Y4732SG-2YWHMXT-MZMWT4B-NOVGTUL-RA6ZOIS-7LJNVRB-3M3SEA3";
     beta.id = "35S5L2L-GQPOM5V-VDK76HQ-DT6FRZJ-ZYIXL36-JYONUE7-M5C33T7-KAS76AK";
+    elbrus.id = "45G57QE-RJX2RHF-HIBZ46W-IMMKIMM-LVA2EJB-YYWX2A4-XDQDSRK-4TOT3QM";
   };
   folders = {
     uni-moodle = {
@@ -17,6 +18,11 @@ let
       devices = [ "beta" "macbook" "realmi8" ];
       id = "default";
       path = "/var/lib/syncthing/nube";
+    };
+    vault = {
+      devices = [ "beta" "elbrus" "macbook" "realmi8" ];
+      id = "default";
+      path = "/var/lib/syncthing/vault";
     };
     android-camara = {
       devices = [ "macbook" "realmi8" ];
