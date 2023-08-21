39ac5521b1a1a2b5b2992a6322c0538488003225
haztecaso <adrianlattes@disroot.org>
2023-08-21 19:25:56 +0200

    meshnodes

---
+++ meshNodes.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }: 
+{
+  custom.mesh.nodes = {
+    lambda = { 
+      publicIp = "185.215.164.95"; 
+      ip = "10.0.0.1";
+      Ed25519PublicKey = "nNqnvCbJyny153B7P3sW108RrnJ0rpOXDuttYIqfb+M";
+      rsaPublicKey = ''
+        MIICCgKCAgEA1jd6FRtcohz95Gk5Iceh6l7fbFDYgN445CT/+RbVl2TgQu4nK/HJ
+        +dQIOtcrxG99yfyQPg5zksar4gJXpteupJ1KmHs0RyD/0ZyUOcGHrU2C0dVSHX6w
+        ktOEoaZRO7C0pqR1OSsZsYwUmrO+XavV8PMoh0slcIWEVcVDZ22Xfe9wVvrI/1gB
+        z8uX4zv8kh9HSpmrSnUlaQvIAOZ/A60WxYHhPweCpvwWZNjKYQ026ssE/bwXXBET
+        uOlim+0DtRYJY04Z3qERoWq/VwVYB5BagqDdFduiE+jPXHT/4Q/VD9NCivPtAPRk
+        +oY5USoVb8axiy65OIpAnsi86d3dZgUHfIFcM8PHxKz/PhPeTCeUmxybgDa+L2WF
+        ZI/hj8MYUdSOOoF2DT04Ua8HYOVDqDUFRZTGPP9Sk/Q5zJN9iBeUlhzjZ/jheE1C
+        eCyo3YhK9hvOy1aEeaRGgG9zKxO3cuSaAJdgwi+05biSViAniyGeYg+y6gAIOob3
+        Qh7orW+wADuy9g9b6bAGW8HLa5rWUchyuqk/j6V0db5rwNTVkJOBcl2B7Ol4CnJB
+        jqi6SAThFQM1jJy7NZe4H9Do701cfCO6vpB7KyKksrxQfFJBB45n0dxFt/V6Z4pI
+        uj36F4x2VjVj8nhmlxJMMO6hDtuiPsOLJUC8kY5C0eU9vIaBbxgGlqkCAwEAAQ==
+      '';
+    };
+    nas = { 
+      ip = "10.0.0.2";
+      connectTo = "lambda";
+      Ed25519PublicKey = "6+O8yM8bm6UK+/BvZ/67DBr1Fw0hn+mm++Dh3aFxYoK";
+      rsaPublicKey = ''
+        MIICCgKCAgEApNowJQQb5WekH09RK/jkNDTza9vbjyFJ5b2mTz2/hKXVYY1ZDK5H
+        WWrdsY3fvf7iUAe1VrWGv6NSJ/rP/iclGPdRaDJoGaJppUDpw8uDHET5tkV1DC72
+        SybJqvInqXljSxo0qyVgj2d1DVmdNnGNSj/h/KeDZXpJOezSoNgGvtnOuBytkdZZ
+        35Jfy9AGuph7PFxFU28dZ/OXjP9psqmRXZ/DrCpF4NNj6FwkFl52dfJ9dP8Pwfkq
+        LQ8kWITwVePCMUFXygi7bKjJkIYh6po+nsYb0vIPCswAlvGSfukRcH1wvd3YlZDL
+        At0FnwEfakN6ICveWxUlsPLip4pw5tc4cp4gPGasISKMJqMTjSrk8x6jE/IU+Zom
+        ynOFFfTZI9Fh+okP84kKSG51cj9Y9IrtKrYCq0VPWqy4r3UPQFlg7062rsWBcs1l
+        4P/VWILIFLAq7XMm1jiGgdmfFqgo6n+KUX5C5RWy3QkO7JeoqX8YT7V57UwwdgSO
+        h4baYUf+EHn5ve1GWymd4hNCmomWhliozN9058/IbcG3qfAMhXtaDoPLPpOV6MPA
+        a1z2pUX804Uao/eKvkbFt5/3i5uGK53OPcV0dsB/inydqOWwtr8Av7cci76yldfj
+        3qMSiUBbCmVXF1vD++goAkjO9rqreeHDOGCMYksL+alXYkftq3jDfMMCAwEAAQ==
+      '';
+    };
+  };
+}
