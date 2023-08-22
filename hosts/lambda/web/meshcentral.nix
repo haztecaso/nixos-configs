414ee95189b32bc4620a46a0453cea99e3d7160b
haztecaso <adrianlattes@disroot.org>
2023-08-22 16:13:36 +0200

    fixing meshcentral

--- hosts/lambda/web/meshcentral.nix
+++ hosts/lambda/web/meshcentral.nix
@@ -2,13 +2,13 @@
 { config, lib, pkgs, ... }: {
   services.nginx = {
     virtualHosts = {
-      ulogger = {
+      meshcentral = {
         enableACME = true;
         forceSSL = true;
         serverName = "meshcentral.haztecaso.com";
         extraConfig = ''
           error_log syslog:server=unix:/dev/log debug;
-          access_log syslog:server=unix:/dev/log,tag=ulogger;
+          access_log syslog:server=unix:/dev/log,tag=meshcentral;
         '';
         locations."/" = {
           proxyPass = "http://nas:4001";
