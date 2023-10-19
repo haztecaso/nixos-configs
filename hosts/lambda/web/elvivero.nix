e251d2b465bff8b0da6c1f0bf8aede4e841a5ab1
haztecaso <adrianlattes@disroot.org>
2023-10-19 12:34:22 +0200

    elvivero wordpress dev

--- hosts/lambda/web/elvivero.nix
+++ hosts/lambda/web/elvivero.nix
@@ -2,6 +2,7 @@
 let
   root = "/var/www/elvivero.es";
   host = "elvivero.es";
+  app  = "wpelvivero";
 in
 {
   security.acme.certs."${host}" = {
@@ -11,36 +12,117 @@ in
     extraDomainNames = [ "*.${host}" ];
   };
   services = {
-    nginx.virtualHosts = {
-      "*.${host}" = {
-        serverName = "*.${host}";
-        useACMEHost = host;
-        addSSL = true;
-        locations."/".return = "301 https://${host}$request_uri";
+    nginx = {
+      upstreams."php-${app}" = {
+        servers = {
+          "unix:${config.services.phpfpm.pools.${app}.socket}" =  {};
+        };
       };
-      "${host}" = {
-        useACMEHost = host;
-        forceSSL = true;
-        root = root;
-        extraConfig = ''
-          expires 1d;
-          error_page 404 /404.html;
-          error_log syslog:server=unix:/dev/log debug;
-          access_log syslog:server=unix:/dev/log,tag=elvivero;
-        '';
-      };
-      "www.${host}" = {
-        useACMEHost = host;
-        forceSSL = true;
-        locations."/".return = "301 https://elvivero.es$request_uri";
-      };
-      "cloud.elvivero.es" = {
-        useACMEHost = host;
-        forceSSL = true;
-        locations."/".proxyPass = "http://nas:8888";
+      virtualHosts = {
+        "*.${host}" = {
+          serverName = "*.${host}";
+          useACMEHost = host;
+          addSSL = true;
+          locations."/".return = "301 https://${host}$request_uri";
+        };
+        "${host}" = {
+          useACMEHost = host;
+          forceSSL = true;
+          root = "${root}-static";
+          extraConfig = ''
+            expires 1d;
+            error_page 404 /404.html;
+            error_log syslog:server=unix:/dev/log debug;
+            access_log syslog:server=unix:/dev/log,tag=elvivero;
+          '';
+        };
+        "www.${host}" = {
+          useACMEHost = host;
+          forceSSL = true;
+          locations."/".return = "301 https://elvivero.es$request_uri";
+        };
+        "cloud.elvivero.es" = {
+          useACMEHost = host;
+          forceSSL = true;
+          locations."/".proxyPass = "http://nas:8888";
+        };
+        "dev.${host}" = {
+          useACMEHost = host;
+          forceSSL = true;
+          root = root;
+          extraConfig = ''
+            index index.php index.html;
+            error_log syslog:server=unix:/dev/log debug;
+            access_log syslog:server=unix:/dev/log,tag=${app};
+            client_max_body_size 20M;
+          '';
+          locations = {
+            "/".extraConfig = ''
+              try_files $uri $uri/ /index.php?$args;
+            '';
+            "~ \.php$".extraConfig = ''
+              fastcgi_split_path_info ^(.+\.php)(/.+)$;
+              fastcgi_intercept_errors on;
+              fastcgi_pass php-${app};
+              include ${pkgs.nginx}/conf/fastcgi_params;
+              include ${pkgs.nginx}/conf/fastcgi.conf;
+              fastcgi_param  SCRIPT_FILENAME $document_root$fastcgi_script_name;
+            '';
+            "~* \.(js|css|png|jpg|jpeg|gif|ico)$".extraConfig = ''
+              expires max;
+              log_not_found off;
+            '';
+            "/favicon.ico".extraConfig = ''
+              log_not_found off;
+              access_log off;
+            '';
+            "/robots.txt".extraConfig = ''
+              allow all;
+              log_not_found off;
+              access_log off;
+            '';
+          };
+        };
       };
     };
+    phpfpm.pools.${app} = {
+      user = app;
+      settings = {
+        "listen.owner" = config.services.nginx.user;
+        "pm" = "dynamic";
+        "pm.max_children" = 32;
+        "pm.max_requests" = 500;
+        "pm.start_servers" = 2;
+        "pm.min_spare_servers" = 2;
+        "pm.max_spare_servers" = 5;
+        "php_admin_value[error_log]" = "stderr";
+        "php_admin_flag[log_errors]" = true;
+        "catch_workers_output" = true;
+      };
+      phpOptions = ''
+        upload_max_filesize = 20M
+        post_max_size = 25M
+      '';
+      phpEnv."PATH" = lib.makeBinPath [ pkgs.php ];
+    };
+    mysql = {
+      enable = true;
+      ensureDatabases = [ app ];
+      ensureUsers = [
+        {
+          name = app;
+          ensurePermissions = { "${app}.*" = "ALL PRIVILEGES"; };
+        }
+      ];
+    };
+    mysqlBackup.databases = [ app ];
   };
+  users.users.${app} = {
+    isSystemUser = true;
+    home = root;
+    group  = app;
+  };
+  users.groups.${app} = {};
   home-manager.sharedModules = [{
     custom.shortcuts.paths.we = root;
   }];
