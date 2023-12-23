ec5561fb23f3c7e5ee4a42d78c7b975583ef85b5
haztecaso <adrianlattes@disroot.org>
2023-12-23 18:57:34 +0100

    wplay + enelpetirrojo.com

---
+++ hosts/lambda/web/enelpetirrojo.nix
@@ -0,0 +1,38 @@
+{ config, lib, pkgs, ... }:
+let
+  root = "/var/www/enelpetirrojo.com";
+  host = "enelpetirrojo.com";
+  app = "enelepetirrojo";
+in
+{
+  security.acme.certs."${host}" = {
+    dnsProvider = "cloudflare";
+    credentialsFile = config.age.secrets."cloudflare".path;
+    group = "nginx";
+    extraDomainNames = [ "*.${host}" ];
+  };
+  services = {
+    nginx = {
+      virtualHosts = {
+        "*.${host}" = {
+          serverName = "*.${host}";
+          useACMEHost = host;
+          addSSL = true;
+          locations."/".return = "301 https://${host}$request_uri";
+        };
+        "${host}" = {
+          useACMEHost = host;
+          forceSSL = true;
+          root = "${root}";
+          extraConfig = ''
+            expires 1d;
+            error_page 404 /404.html;
+            error_log syslog:server=unix:/dev/log debug;
+            access_log syslog:server=unix:/dev/log,tag=enelpetirrojo;
+            try_files $uri $uri.html $uri/ =404;
+          '';
+        };
+      };
+    };
+  };
+}
