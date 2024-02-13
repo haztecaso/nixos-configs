3a33fede7f0b2601a0cb3f46c440d6df8818babf
haztecaso <adrianlattes@disroot.org>
2024-02-13 11:11:21 +0100

    removing old gitea module

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -11,70 +11,10 @@ in
   ];
 
   options.custom.services = with lib; {
-    gitea.enable = mkEnableOption "Enable gitea code hosting.";
     netdata.enable = mkEnableOption "Enable netdata web panel.";
   };
 
   config = with lib; mkMerge [
-    # gitea config
-    (mkIf cfg.gitea.enable (
-      let
-        cfg = config.custom.services.gitea;
-        port = 8003;
-        serverName = "git.haztecaso.com";
-      in
-      {
-        services = {
-          gitea = {
-            enable = true;
-            appName = "Gitea";
-            settings = {
-              server = {
-                DOMAIN = serverName;
-                HTTP_ADDR = "127.0.0.1";
-                HTTP_PORT = port;
-                ROOT_URL = "https://${serverName}/";
-              };
-              service = {
-                DISABLE_REGISTRATION = true;
-              };
-              session = {
-                COOKIE_SECURE = true;
-              };
-              security = {
-                LOGIN_REMEMBER_DAYS = 14;
-                MIN_PASSWORD_LENGTH = 12;
-                PASSWORD_COMPLEXITY = "lower,upper,digit,spec";
-                PASSWORD_CHECK_PWN = true;
-              };
-              mailer = {
-                ENABLED = true;
-                MAILER_TYPE = "sendmail";
-                FROM = "gitea@haztecaso.com";
-                SENDMAIL_PATH = "${pkgs.postfix}/bin/sendmail";
-              };
-              other = {
-                SHOW_FOOTER_VERSION = false;
-                SHOW_FOOTER_TEMPLATE_LOAD_TIME = false;
-              };
-            };
-          };
-          nginx.virtualHosts.gitea = {
-            enableACME = true;
-            forceSSL = true;
-            serverName = serverName;
-            locations."/".proxyPass = "http://127.0.0.1:${toString port}";
-          };
-        };
-        # mailserver.loginAccounts."gitea@haztecaso.com" = {
-        #   hashedPassword = "$2y$05$YHivXvJqLKBlXuddFkGpiev087Ve0DsJwyvKJFUsEfB31ULkZPS5O";
-        #   name = "Gitea";
-        #   sendOnly = true;
-        # };
-      }
-    ))
-
-    # netdata config
     (mkIf cfg.netdata.enable (
       let
         cfg = config.custom.services.netdata;
