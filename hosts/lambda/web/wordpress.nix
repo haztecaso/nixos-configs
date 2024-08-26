9e0c2dae01e18a00cbc9c34e503b96eeb4ac67f3
haztecaso <adrianlattes@disroot.org>
2024-08-26 20:53:10 +0200

    format

--- hosts/lambda/web/wordpress.nix
+++ hosts/lambda/web/wordpress.nix
@@ -1,49 +1,14 @@
 { config, lib, pkgs, ... }:
 {
   services.custom.web.wordpress.sites = {
-    wplay = {
-      appName = "wplay";
-      domain = "haztecaso.com";
-      subdomain = "wplay";
-    };
-    elvivero = {
-      appName = "wpelvivero";
-      domain = "elvivero.es";
-    };
-    bicibufanda = {
-      appName = "bicibufanda";
-      domain = "bufanda.cc";
-      subdomain = "bici";
-      max_upload_filesize = 200;
-    };
-    claudiogabis = {
-      appName = "wpclau";
-      domain = "claudiogabis.com";
-      max_upload_filesize = 400;
-    };
-    delunesadomingo = {
-      appName = "wpleandro";
-      domain = "delunesadomingo.es";
-    };
-    colchonreview = {
-      appName = "wpcolchon";
-      domain = "colchon.review";
-      max_upload_filesize = 1024;
-    };
-    equisoain = {
-      appName = "wpequisoain";
-      domain = "equisoain.com";
-    };
-    olaborathorio = {
-      appName = "wpolaborathorio";
-      domain = "elvivero.es";
-      subdomain = "olaborathorio";
-      root = "/var/www/olaborathorio";
-    };
-    zulmarecchini = {
-      appName = "wpzulma";
-      domain = "zulmarecchini.com";
-      max_upload_filesize = 350;
-    };
+    bicibufanda = { appName = "bicibufanda"; domain = "bufanda.cc"; subdomain = "bici"; max_upload_filesize = 200; };
+    claudiogabis = { appName = "wpclau"; domain = "claudiogabis.com"; max_upload_filesize = 400; };
+    colchonreview = { appName = "wpcolchon"; domain = "colchon.review"; max_upload_filesize = 1024; };
+    delunesadomingo = { appName = "wpleandro"; domain = "delunesadomingo.es"; };
+    elvivero = { appName = "wpelvivero"; domain = "elvivero.es"; };
+    equisoain = { appName = "wpequisoain"; domain = "equisoain.com"; };
+    olaborathorio = { appName = "wpolaborathorio"; domain = "elvivero.es"; subdomain = "olaborathorio"; };
+    wplay = { appName = "wplay"; domain = "haztecaso.com"; subdomain = "wplay"; };
+    zulmarecchini = { appName = "wpzulma"; domain = "zulmarecchini.com"; max_upload_filesize = 350; };
   };
 }
