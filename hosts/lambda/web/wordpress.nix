6ff12356da0e796aca5104b01058bc8183515c34
haztecaso <adrianlattes@disroot.org>
2024-10-29 23:17:39 +0100

    updating interface of custom wordpress module

--- hosts/lambda/web/wordpress.nix
+++ hosts/lambda/web/wordpress.nix
@@ -1,14 +1,49 @@
 { config, lib, pkgs, ... }:
 {
   services.custom.web.wordpress.sites = {
-    bicibufanda = { appName = "bicibufanda"; domain = "bufanda.cc"; subdomain = "bici"; max_upload_filesize = 200; };
-    claudiogabis = { appName = "wpclau"; domain = "claudiogabis.com"; max_upload_filesize = 400; };
-    colchonreview = { appName = "wpcolchon"; domain = "colchon.review"; root = "/var/www/colchon.review"; max_upload_filesize = 1024; };
-    delunesadomingo = { appName = "wpleandro"; domain = "delunesadomingo.es"; };
-    elvivero = { appName = "wpelvivero"; domain = "elvivero.es"; };
-    equisoain = { appName = "wpequisoain"; domain = "equisoain.com"; };
-    olaborathorio = { appName = "wpolaborathorio"; domain = "elvivero.es"; subdomain = "olaborathorio"; };
-    wplay = { appName = "wplay"; domain = "haztecaso.com"; subdomain = "wplay"; };
-    zulmarecchini = { appName = "wpzulma"; domain = "zulmarecchini.com"; max_upload_filesize = 350; };
+    bicibufanda = { 
+      appName = "bicibufanda"; 
+      host = "bici.bufanda.cc"; 
+      ACMEHost = "bufanda.cc"; 
+      max_upload_filesize = 200; 
+    };
+    claudiogabis = { 
+      appName = "wpclau"; 
+      host = "claudiogabis.com"; 
+      max_upload_filesize = 400; 
+    };
+    colchonreview = { 
+      appName = "wpcolchon"; 
+      host = "colchon.review"; 
+      root = "/var/www/colchon.review"; 
+      max_upload_filesize = 1024; 
+    };
+    delunesadomingo = { 
+      appName = "wpleandro"; 
+      host = "delunesadomingo.es"; 
+    };
+    elvivero = { 
+      appName = "wpelvivero"; 
+      host = "elvivero.es"; 
+    };
+    equisoain = { 
+      appName = "wpequisoain"; 
+      host = "equisoain.com"; 
+    };
+    olaborathorio = { 
+      appName = "wpolaborathorio"; 
+      host = "olaborathorio.elvivero.es"; 
+      ACMEHost = "elvivero.es"; 
+    };
+    wplay = { 
+      appName = "wplay"; 
+      host = "wplay.haztecaso.com"; 
+      ACMEHost = "haztecaso.com"; 
+    };
+    zulmarecchini = { 
+      appName = "wpzulma"; 
+      host = "zulmarecchini.com"; 
+      max_upload_filesize = 350; 
+    };
   };
 }
--- modules/web/wordpress.nix
+++ modules/web/wordpress.nix
@@ -1,8 +1,9 @@
 { config, lib, pkgs, ... }:
 #TODO: 
+# nginx caching
 # automate some installation and config processes:
-# - Create root folder if not exists and add install script that does the
-# following (using wp-cli)
+# detect hostname changes (bind hostname to wordpress conifg somehow)
+# - Create root folder if not exists and add install script that does the following (using wp-cli)
 #   - helps setting mysql password
 #   - downloads latest wordpress
 #   - fills up wp-config.php
@@ -20,21 +21,10 @@ let
           # TODO: assertions: appName should be only alpha characters
           # mirar type.strMatching
         };
-        domain = mkOption {
-          type = types.str;
-          example = "myweb.com";
-          description = "Website domain";
-        };
-        subdomain = mkOption {
-          type = types.nullOr types.str;
-          example = "wp";
-          description =
-            "Subdomain where to install wordpress. Main domain if null.";
-        };
         host = mkOption {
           type = types.str;
-          readOnly = true;
-          description = "Concatenation of subdomain with domain.";
+          example = "sub.domain.com";
+          description = "The hostname of the wordpress site.";
         };
         root = mkOption {
           type = types.str;
@@ -43,7 +33,10 @@ let
         };
         ACMEHost = mkOption {
           type = types.str;
-          description = "The ACME host to use for SSL.";
+          description = ''
+            The ACME host to use for SSL. Defaults to the wordpress host (change 
+            if your wordpress is installed on a subdomain).
+          '';
         };
         max_upload_filesize = mkOption {
           type = types.numbers.positive;
@@ -52,15 +45,10 @@ let
         };
       };
       config = {
-        subdomain = lib.mkDefault null;
         # TODO: use submodule attr name
         # appName = lib.mkDefault "wp${cfg.name}";
         root = lib.mkDefault "/var/www/wordpress/${cfg.host}";
-        host = if cfg.subdomain == null then
-          cfg.domain
-        else
-          "${cfg.subdomain}.${cfg.domain}";
-        ACMEHost = lib.mkDefault cfg.domain;
+        ACMEHost = lib.mkDefault cfg.host;
       };
     };
 in {
