8b3d51cf5c66dcc7fd998808c61935ab50d495ae
haztecaso <adrianlattes@disroot.org>
2024-10-30 18:00:15 +0100

    fail2ban for navidrome

--- hosts/nas/navidrome.nix
+++ hosts/nas/navidrome.nix
@@ -10,6 +10,27 @@
           AuthWindowLength = "60s";
       };
   };
+  services.fail2ban = {
+    enable = true;
+    jails.navidrome = {
+      filter = {
+        INCLUDES.before = "common.conf";
+        Definition = {
+          failregex = "^.*msg=\"Unsuccessful login\".*X-Real-Ip:\[<HOST>\]";
+          ignoreregex = "";
+        }
+      };
+      settings = {
+        backend = "systemd";
+        port = "80,443";
+        filter = "navidrome[journalmatch='_SYSTEMD_UNIT=navidrome.service']";
+        banaction = "%(banaction_allports)s";
+        maxretry = 5;
+        bantime = 14400;
+        findtime = 14400;
+      };
+    };
+  };
   networking.firewall.allowedTCPPorts = [ 4747 ];
   users.users.navidrome = {
       isSystemUser = true;
